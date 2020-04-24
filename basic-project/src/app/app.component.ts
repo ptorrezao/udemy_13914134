22cf4d25459bd5f3d1a16c91cbe19a50da018c8e
Pedro Torrezao <pedro.torrezao@gmail.com>
2020-04-25 00:49:00 +0100

    Assignment

--- basic-project/src/app/app.component.ts
+++ basic-project/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`h3 { color: red;}`]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username='asda';
 }
