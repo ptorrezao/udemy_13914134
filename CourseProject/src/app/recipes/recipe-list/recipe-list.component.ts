ab7bacca47d3c626a7aa70a946d3d298dd930377
Pedro Torrezao <pedro.torrezao@gmail.com>
2020-04-26 16:11:25 +0100

    RecipeList

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Lasanha', 'Lasanha é feita com essas fitas colocadas em camadas, e entremeadas com recheio e molho.', 'https://www.pingodoce.pt/wp-content/uploads/2015/03/lasanha-617-516x310.jpg'),
+    new Recipe('Salada César', 'Salada César é uma salada preparada com alface romana e molho Caesar.', 'https://www.pingodoce.pt/wp-content/uploads/2019/07/salada-cesar-com-croutons-1.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
