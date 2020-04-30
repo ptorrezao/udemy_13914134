ca24dd9d62af8befdd5b37ecfcccfbb4dd5a776f
Pedro Torrezao <pedro.torrezao@gmail.com>
2020-05-01 00:07:21 +0100

    Add selection

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,12 +8,17 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Lasanha', 'Lasanha é feita com essas fitas colocadas em camadas, e entremeadas com recheio e molho.', 'https://www.pingodoce.pt/wp-content/uploads/2015/03/lasanha-617-516x310.jpg'),
+    new Recipe('Lasanha', 'Lasanha é feita com essas fitas colocadas em camadas, e entremeadas com recheio e molho. ', 'https://www.pingodoce.pt/wp-content/uploads/2015/03/lasanha-617-516x310.jpg'),
     new Recipe('Salada César', 'Salada César é uma salada preparada com alface romana e molho Caesar.', 'https://www.pingodoce.pt/wp-content/uploads/2019/07/salada-cesar-com-croutons-1.jpg')
   ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onSelectedRecipe(recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
